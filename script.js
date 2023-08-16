6aeb49f6b23ee53757dfbce38aa3c671c7c86254
Syntax Designer <akpanjoseph2021@gmail.com>
2023-08-16 02:50:38 +0100

    stop watch demo application with js

---
+++ script.js
@@ -0,0 +1,24 @@
+let hour = document.querySelector('#hour')
+let min = document.querySelector('#min')
+let sec = document.querySelector('#hour')
+
+const startBtn = document.querySelector('#start')
+const stopBtn = document.querySelector('#stop')
+const resetBtn = document.querySelector('#reset')
+
+
+// document.q('body').addEventListener('onload',()=>{
+//     alert('hello')
+// })
+
+// window.onload(()=> alert('hell'))
+window.onload = ()=>{
+    let hourCount = 0;
+    let minCount = 0;
+    let secCount = 0;
+
+
+    const startTimer = ()=>{
+        
+    }
+}
